8d23e0c58cf98cc49254284312d0800f325f85e7
TaWeiTu <tu.da.wei@gmail.com>
2017-09-05 23:54:15 +0800

    gotta be worse, right?

--- CodeForces/g.cpp
+++ CodeForces/g.cpp
@@ -7,22 +7,9 @@ int main() {
     ios_base::sync_with_stdio(false); cin.tie(0);
     srand(time(nullptr));
     freopen("in.txt", "w", stdout);
-    /* 
-    for (int i = 1; i <= 200000; ++i) {
-        cout << (long long)rand() * (long long)rand() % 200000 + 1 << ' ';
-    }
-    for (int i = 2; i <= 200000; ++i) {
-        cout << rand() % (i - 1) + 1 << ' ' << i << endl;
-    } */
-    cout << 200000 << ' ' << 100000 << endl;
+    cout << 200000 << ' ' << 10487 << ' ' << 17788 << endl;
     for (int i = 0; i < 200000; ++i) {
-        char c = 'a' + (i & 1);
-        cout << "1-" << c << ' ';
-    }
-    cout << endl;
-    for (int i = 0; i < 100000; ++i) {
-        char c = 'a' + (i & 1);
-        cout << "1-" << c << ' ';
+        cout << (long long)rand() * (long long)rand() % 1000000 + 1 << ' ';
     }
     cout << endl;
     return 0;

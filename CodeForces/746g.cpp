27801a5bd803ed631ad0e54670766af9d9b1bc66
TaWeiTu <tu.da.wei@gmail.com>
2017-10-16 00:03:23 +0800

    Codeforces round #440 Div1 -3

--- CodeForces/746g.cpp
+++ CodeForces/746g.cpp
@@ -1,40 +1,56 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
-const int MAXN = 200000 + 5;
-int n, t, k, a[MAXN], id, leaf;
-vector<int> T[MAXN], R[MAXN], L[MAXN];
-void DFS1(int);
-void DFS2(int);
+const int maxn = 2e5 + 10;
+int a[maxn], deg[maxn], d[maxn];
+vector<int> node[maxn], G[maxn];
 
 int main() {
-  cin.tie(0); ios_base::sync_with_stdio(false);
-  cin >> n >> t >> k;
-  for (int i = 1; i <= n; ++i) cin >> a[i];
-  id = 2;
-  L[0].push_back(1);
-  for (int i = 1; i <= n; ++i) {
-    for (int j = 0; j < a[i]; ++j) {
-      L[i].push_back(id++);
+    ios_base::sync_with_stdio(false); cin.tie(0);
+    int n, t, k; cin >> n >> t >> k;
+    int leaf = n - t;
+    if (leaf < k) return cout << "-1" << endl, 0;
+    for (int i = 1; i <= t; ++i) cin >> a[i];
+    int p = 1, now = 2;
+    d[1] = 0;
+    for (int i = 1; i <= t; ++i) {
+        int np = -1;
+        for (int j = 0; j < a[i]; ++j) {
+            if (np == -1) np = now;
+            d[now] = d[p] + 1;
+            node[d[now]].push_back(now);
+            ++deg[p]; ++deg[now];
+            G[p].push_back(now);
+            ++now;
+        }
+        p = np;
     }
-    int k = 0;
-    for (int j = 0; j < L[i].size(); ++j) {
-      T[L[i - 1][k]].push_back(L[i][j]);
-      R[L[i][j]].push_back(L[i - 1][k]);
-      k++;
-      if (k == L[i - 1].size()) k = 0;
+    int curdep = t - 1;
+    size_t ptr = 0, rptr = node[curdep].size() - 1;
+    for (int i = 1; i <= t; ++i) sort(node[i].begin(), node[i].end(), [&](const int& a, const int& b) { return deg[a] > deg[b]; });
+    while (leaf > k) {
+        if (curdep == 0) return cout << "-1" << endl, 0;
+        while (ptr < rptr) {
+            if (leaf <= k) break;
+            int u = node[curdep][ptr], b = node[curdep][rptr];
+            if (deg[u] <= 2) {
+                ++ptr;
+                continue;
+            }
+            if (deg[b] >= 2) {
+                rptr = -1;
+                continue;
+            }
+            int v = G[u].back(); G[u].pop_back();
+            --deg[u]; ++deg[b];
+            G[b].push_back(v);
+            --leaf; --rptr;
+        }
+        if (ptr >= rptr) ptr = 0, --curdep, rptr = node[curdep].size() - 1;
     }
-  }
-  DFS1(1);
-  DFS2(1);
-}
-
-void DFS1(int x) {
-  if (T[x].size() == 0) leaf++;
-  for (int c : T[x]) DFS(c);
-}
-
-void DFS2(int x) {
-  for (int c : T[x])
+    cout << n << endl;
+    for (int i = 1; i <= n; ++i) {
+        for (int u : G[i]) cout << i << ' ' << u << endl;
+    }
+    return 0;
 }
--- CodeForces/g.cpp
+++ CodeForces/g.cpp
@@ -4,17 +4,24 @@ using namespace std;
 typedef long long ll;
 
 set<pair<int, int>> s;
-const int maxn = 100000;
+const int maxn = 1e5;
 
 template <typename T> T rnd(T l, T r) {
-    return rand() * 1ll * rand() % (r - l) + l;
+    return rand() * 1ll * rand() % (r - l + 1) + l;
 }
 
 int main() {
     ios_base::sync_with_stdio(false); cin.tie(0);
     srand(time(nullptr));
     freopen("in.txt", "w", stdout);
-    cout << maxn << endl;
-    for (int i = 1; i <= maxn; ++i) cout << rnd(1, maxn) << ' '; cout << endl;
+    // cout << maxn << endl;
+    // for (int i = 1; i <= maxn; ++i) cout << rnd(1, maxn) << ' '; cout << endl;
+    cout << maxn << ' ' << maxn << endl;
+    for (int i = 0; i < maxn; ++i) cout << rnd(1, maxn) << ' '; cout << endl;
+    for (int i = 0; i < maxn; ++i) {
+        int t = rnd(0, 1); cout << t << ' ';
+        if (t == 0) cout << rnd(1, maxn) << ' ' << rnd(1, maxn) << endl;
+        else cout << rnd(1, maxn) << endl;
+    }
     return 0;
 }
